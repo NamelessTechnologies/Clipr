0d8db580ee3023fb4199a675a21d36faa1ea53b2
Christopher Lo <christopherlo383@gmail.com>
2024-09-20 03:36:50 -0700

    set up TagTest

---
+++ frontend/src/pages/TagTest.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import Saved_Temp from "../types/Saved_Temp";
+
+function TagTest() {
+
+  const [data, setData] = useState<Saved_Temp[]>([]); 
+  const url = 'http://localhost:5001/';
+
+  const fetchSaves = async () => {
+    try {
+        const response = await fetch(url + 'saved/temp');
+        const json = await response.json();
+        const saves: Saved_Temp[] = [];
+        json.forEach((post: any) => {
+            const Save: Saved_Temp = {
+                post_id: post.postID,
+                user_id: post.userID
+            };        
+            saves.push(Save);
+        });
+      setData(saves); 
+    } catch (error) {
+        console.error(error);
+        throw new Error("Error getting saves data");
+    }
+}
+
+  useEffect(() => {
+    fetchSaves();
+  }, []);
+
+    return (
+      <>
+      <hr></hr>
+      <h2 className="table_title">Save Table:</h2>
+        <table>
+          <tr>
+            <th>post_id</th>
+            <th>user_id</th>
+          </tr>
+  
+          {data?.map(user => (
+            <tr>
+              <td key={user.user_id}>{user.user_id}</td>
+              <td key={user.user_id}>{user.post_id}</td>
+            </tr>
+          ))}
+        </table>
+      </>
+    );
+  }
+  
+export default TagTest;
