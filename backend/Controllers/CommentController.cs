ac0a31576c70c7014d4826c9745847667851a514
Christopher Lo <christopherlo383@gmail.com>
2024-09-20 02:55:54 -0700

    Revert "Completed CommentController methods and adjusted Comment model"
    This reverts commit 3be7038f8cdc6f96ffbfa9e1e2551377ead7eb0a.
    
    mb team

--- backend/Controllers/CommentController.cs
+++ backend/Controllers/CommentController.cs
@@ -15,12 +15,6 @@ public class CommentController : ControllerBase {
         conn = DBConn.Instance().getConn();
     }
 
-    private NpgsqlConnection conn;
-
-    public CommentController(){
-        conn = DBConn.Instance().getConn();
-    }
-
     [HttpGet("{id}")]
     public IActionResult getComment(int id) {
         // Define your SQL query (e.g., retrieving a single value from a specific column)
@@ -39,46 +33,13 @@ public class CommentController : ControllerBase {
         // THIS DONT WORK YET
         if (reader.Read()) {
             return Ok(new Comment {
-                ID = reader.GetInt32(0),
-                ParentID = reader.GetInt32(1),
-                PostID = reader.GetInt32(2),
-                UserID = reader.GetInt32(3),
-                Message = reader.GetString(4)
+                // ID = reader.GetInt32(0),
+                // UserID = reader.GetInt32(1),
+                
             });
         }  else {
             return BadRequest("no data");
         }
-
-    }
-
-    [HttpGet]
-    public IActionResult getAllComments() {
-        var sql = "SELECT * FROM comment";
-
-        if (conn.State != System.Data.ConnectionState.Open) {
-            conn.Open();
-        }
-
-        using var cmd = new NpgsqlCommand(sql, conn);
-        var reader = cmd.ExecuteReader();
-
-        var comments = new List<Comment>();
-
-        if (!reader.HasRows) {
-            return BadRequest("no data");
-        }
-
-        while (reader.Read()) {
-            Comment newComment = new Comment {
-                ID = reader.GetInt32(0),
-                ParentID = reader.GetInt32(1),
-                PostID = reader.GetInt32(2),
-                UserID = reader.GetInt32(3),
-                Message = reader.GetString(4)
-            };
-            comments.Add(newComment);
-        }
-        return Ok(comments);
 
     }
 }
--- backend/Models/Comment.cs
+++ backend/Models/Comment.cs
@@ -4,13 +4,7 @@ public class Comment
 {
     public int? ID { get; set; }
 
-    public int? ParentID { get; set; }
-
-    public int? PostID { get; set; }
-
     public int? UserID { get; set; }
 
-    public string? Message { get; set; }
-
-
+    public int? ServerID { get; set; }
 }
