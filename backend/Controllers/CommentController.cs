3be7038f8cdc6f96ffbfa9e1e2551377ead7eb0a
Cesar de Paula <cdepaula942@gmail.com>
2024-09-20 02:20:05 -0700

    Completed CommentController methods and adjusted Comment model

--- backend/Controllers/CommentController.cs
+++ backend/Controllers/CommentController.cs
@@ -10,6 +10,12 @@ namespace backend.Controllers;
 
 public class CommentController : ControllerBase {
 
+    private NpgsqlConnection conn;
+
+    public CommentController(){
+        conn = DBConn.Instance().getConn();
+    }
+
     [HttpGet("{id}")]
     public IActionResult getComment(int id) {
 
@@ -33,13 +39,46 @@ public class CommentController : ControllerBase {
         // THIS DONT WORK YET
         if (reader.Read()) {
             return Ok(new Comment {
-                // ID = reader.GetInt32(0),
-                // UserID = reader.GetInt32(1),
-                
+                ID = reader.GetInt32(0),
+                ParentID = reader.GetInt32(1),
+                PostID = reader.GetInt32(2),
+                UserID = reader.GetInt32(3),
+                Message = reader.GetString(4)
             });
         }  else {
             return BadRequest("no data");
         }
+
+    }
+
+    [HttpGet]
+    public IActionResult getAllComments() {
+        var sql = "SELECT * FROM comment";
+
+        if (conn.State != System.Data.ConnectionState.Open) {
+            conn.Open();
+        }
+
+        using var cmd = new NpgsqlCommand(sql, conn);
+        var reader = cmd.ExecuteReader();
+
+        var comments = new List<Comment>();
+
+        if (!reader.HasRows) {
+            return BadRequest("no data");
+        }
+
+        while (reader.Read()) {
+            Comment newComment = new Comment {
+                ID = reader.GetInt32(0),
+                ParentID = reader.GetInt32(1),
+                PostID = reader.GetInt32(2),
+                UserID = reader.GetInt32(3),
+                Message = reader.GetString(4)
+            };
+            comments.Add(newComment);
+        }
+        return Ok(comments);
 
     }
 }
--- backend/Models/Comment.cs
+++ backend/Models/Comment.cs
@@ -4,7 +4,13 @@ public class Comment
 {
     public int? ID { get; set; }
 
+    public int? ParentID { get; set; }
+
+    public int? PostID { get; set; }
+
     public int? UserID { get; set; }
 
-    public int? ServerID { get; set; }
+    public string? Message { get; set; }
+
+
 }
