8e2897d463a94d7c47ac5b10b0e5ee0906f3ac4e
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2024-09-27 01:01:12 -0700

    added navbar and styling using tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,65 +1,42 @@
 import hsr_logo from './assets/hsr_logo.png'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-// import User from './types/User';
-import TableViewUser from './TableViewUser';
-import PostTest from './pages/PostTest';
-import SaveTest from './pages/SaveTest';
-import CommentTest from './pages/CommentTest';
-import TableViewConversation from './TableViewConversation';
-import TableViewMessage from './TableViewMessage';
-import TagTest from './pages/TagTest';
-import LikeTest from './pages/LikeTest';
-import PostTagTest from './pages/PostTagTest';
-import CommentLikeTest from './pages/CommentLikeTest';
-import MediaTemp from './pages/Media';
+import { NavBar } from './components/Navbar';
+import CreateAccount from './pages/CreateAccount';
+import Home from './pages/Home.tsx';
+// import TableViewUser from './TableViewUser';
+// import PostTest from './pages/PostTest';
+// import SaveTest from './pages/SaveTest';
+// import CommentTest from './pages/CommentTest';
+// import TableViewConversation from './TableViewConversation';
+// import TableViewMessage from './TableViewMessage';
+// import TagTest from './pages/TagTest';
+// import LikeTest from './pages/LikeTest';
+// import PostTagTest from './pages/PostTagTest';
+// import CommentLikeTest from './pages/CommentLikeTest';
+// import MediaTemp from './pages/Media';
 
 function App() {
-  // const [data, setData] = useState<User>();
-  // const [url, setURL] = useState<string>("");
-  // let url: string | undefined = "";
 
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  // const fetchData = async (): Promise<string> => {
-  //   try {
-  //     const response = await fetch('http://localhost:5001/TestData/user');
-  //     const json = await response.json() as User;
-  //     console.log("setting URL to " + json.firstName);
-  //     setURL(json.firstName);
-  //     return json.firstName;
-  //     // console.log(json.firstName);
-  //   } catch (error) {
-  //     console.error(error);
-  //     throw new Error("This is a custom error message");
-  //     }
-    
-  // }
   return (
     <>
-      <div>
+
+  <Router>
+        <NavBar/>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/Sign Up" element={<CreateAccount/>} />
+        </Routes>
+    </Router>
+
+      {/* <div>
         <a href="https://hsr.hoyoverse.com/en-us/" target="_blank">
           <img src={hsr_logo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>The Nameless</h1>
-      {/* <div className="card">
-        <button onClick={fetchData}>
-          Fetch Data
-        </button>
 
-        <ul>
-          <li>First Name: {data ? data.firstName : ""}</li>
-          <li>Last Name: {data ? data.lastName : ""}</li>
-          <li>Faction: {data ? data.faction : ""}</li>
-        </ul>
-
-      </div> */}
-
-      {/* {url} */}
       <TableViewUser></TableViewUser>
       <TableViewConversation></TableViewConversation>
       <TableViewMessage></TableViewMessage>
@@ -70,7 +47,7 @@ function App() {
       <LikeTest></LikeTest>
       <PostTagTest></PostTagTest>
       <CommentLikeTest></CommentLikeTest>
-      <MediaTemp></MediaTemp>
+      <MediaTemp></MediaTemp> */}
     </>
   )
 }
