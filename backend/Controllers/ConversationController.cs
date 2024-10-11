1acf66c668ddda916dee5b2014d8ff656a772d99
Christopher Lo <christopherlo383@gmail.com>
2024-10-11 03:24:42 -0700

    feat: creation of new conversations and passing convoid to Messages page

--- backend/Controllers/ConversationController.cs
+++ backend/Controllers/ConversationController.cs
@@ -12,16 +12,17 @@ namespace backend.Controllers;
 [Route("[controller]")]
 public class ConversationController : ControllerBase
 {
-    private NpgsqlConnection conn;
+    // private NpgsqlConnection conn;
     private String connString = "Host=clipr-pg.postgres.database.azure.com;Username=clipr_admin;Password=password123!;Database=clipr_database";
 
-    public ConversationController(){
-        conn = DBConn.Instance().getConn();
-    }
+    // public ConversationController(){
+    //     conn = DBConn.Instance().getConn();
+    // }
 
     [HttpPost]
-    public async void PostConversation([FromBody] Conversation conversation) {
-        var sql = "INSERT INTO conversation (user_id, user_id_2) VALUES(@user_id, @user_id_2);";
+    public async Task<IActionResult> PostConversation([FromBody] Conversation conversation) {
+        try {
+        var sql = "INSERT INTO conversation (user_id, user_id_2) VALUES(@user_id, @user_id_2) returning id;";
 
         using var conn = new NpgsqlConnection(connString);
         if (conn.State != System.Data.ConnectionState.Open) {
@@ -31,13 +32,18 @@ public class ConversationController : ControllerBase
         await using (var cmd = new NpgsqlCommand(sql, conn)) {
             cmd.Parameters.AddWithValue("user_id", conversation.User_id);
             cmd.Parameters.AddWithValue("user_id_2", conversation.User_id_2);
-            await cmd.ExecuteNonQueryAsync();
+            var insertedId = (int) await cmd.ExecuteScalarAsync();
+            // Console.WriteLine("insertedId: " + insertedId);
+            return Ok(new {id = insertedId});
+        }
+        } catch (Exception e) {
+            return (OkObjectResult)StatusCode(500, new { error = e.Message });
         }
     }
 
     [HttpGet]
     public IActionResult GetAllConversationMessages([FromQuery]ConversationMessagesQuery conversation) {
-        var sql = "SELECT * FROM Message WHERE convo_id = (select id FROM Conversation WHERE user_id= " + conversation.User_1 + " and user_id_2= " + conversation.User_2 + ");";
+        var sql = "SELECT * FROM Message WHERE convo_id = (select id FROM Conversation WHERE (user_id= " + conversation.User_1 + " and user_id_2= " + conversation.User_2 + ") or"+"(user_id= " + conversation.User_2 + " and user_id_2= " + conversation.User_1 + "));";
 
         using var conn = new NpgsqlConnection(connString);
         if (conn.State != System.Data.ConnectionState.Open) {
@@ -64,6 +70,36 @@ public class ConversationController : ControllerBase
             messages.Add(message);
         }
         return Ok(messages);
+    }
+
+    [HttpGet("convoid")]
+    public IActionResult GetConvoID([FromQuery]ConversationMessagesQuery conversation) {
+        var sql = "select * FROM Conversation WHERE (user_id= " + conversation.User_1 + " and user_id_2= " + conversation.User_2 + ") or "+"(user_id= " + conversation.User_2 + " and user_id_2= " + conversation.User_1 + ");";
+        using var conn = new NpgsqlConnection(connString);
+        if (conn.State != System.Data.ConnectionState.Open) {
+            conn.Open();
+        }
+
+        using var cmd = new NpgsqlCommand(sql, conn);
+        var reader = cmd.ExecuteReader();
+        Conversation convo = new Conversation();
+
+
+        if (!reader.HasRows) {
+            Conversation bad_convo = new Conversation {
+                Id = -1,
+                User_id = -1,
+                User_id_2 = -1
+            };
+            return BadRequest(bad_convo);
+        }
+
+        while (reader.Read()) {
+            convo.Id = reader.GetInt32(0);
+            convo.User_id = reader.GetInt32(1);
+            convo.User_id_2 = reader.GetInt32(2);
+        }
+        return Ok(convo);
     }
 
     [HttpPost("message")]
