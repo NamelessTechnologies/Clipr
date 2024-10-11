a75d25ebf37686ad8013e02a8973efd8a9b0550a
alonzojp <alonzojp@gmail.com>
2024-10-11 04:30:26 -0700

    Added "go down" button to messages

--- frontend/src/pages/Messages.tsx
+++ frontend/src/pages/Messages.tsx
@@ -92,11 +92,15 @@ const Messages: React.FC =  () => {
 
     const refreshPage = () => {
         window.location.reload();
-        // await scrollToBottom.current?.scrollIntoView({behavior:'smooth'});
+    }
+
+    const goDown = () => {
+        scrollToBottom.current?.scrollIntoView({behavior:'smooth'});
     }
 
     return (
         <div className="flex flex-col justify-center items-center">
+            <button className='fixed bottom-5 right-5 rounded-xl bg-slate-400 p-5' onClick={goDown}>Jump To Recent</button>
             <div className="w-screen p-5 pl-20">
                 <h1 className="text-white text-3xl">{second_user[0]}</h1>
                 {/* <h1 className="text-white text-3xl">{"convoid " + second_user[2]}</h1>                 */}
@@ -112,7 +116,7 @@ const Messages: React.FC =  () => {
 
             <button className='mr-10 text-blue-300 pt-2' onClick={refreshPage}>Refresh</button>
 
-            <form className='flex justify-center w-screen' onSubmit={messagePipeline}>
+            <form className='flex justify-center w-screen pb-10' onSubmit={messagePipeline}>
 
                 <div className='flex flex-col justify-center w-1/3 pr-5'>
                     <input className='p-2 rounded-xl' value={message} onChange={(e) => setMessage(e.target.value)}>
