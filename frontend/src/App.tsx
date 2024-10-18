4399391514c6162c89281653c39261f091050dcd
Christopher Lo <christopherlo383@gmail.com>
2024-10-18 13:48:08 -0700

    modified App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 // import hsr_logo from './assets/hsr_logo.png'
 import './index.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { socket } from './socket.tsx';
+import { ConnectionState } from './components/ConnectionState';
+import { ConnectionManager } from './components/ConnectionManager';
+import { Events } from "./components/Events";
+import { MyForm } from './components/MyForm';
 
 import { NavBar } from './components/Navbar';
 import CreateAccount from './pages/CreateAccount';
@@ -12,7 +18,25 @@ import Messages from './pages/Messages.tsx';
 import SetReceiver from './pages/SetReceiver.tsx';
 
 function App() {
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    const [fooEvents, setFooEvents] = useState([]);
 
+    useEffect(() => {
+        function onConnect() {
+          setIsConnected(true);
+    }
+
+    function onDisconnect() {
+        setIsConnected(false);
+      }
+  
+      function onFooEvent(value: any) {
+        setFooEvents(previous => [...previous, value]);
+      }
+  
+      socket.on('connect', onConnect);
+      socket.on('disconnect', onDisconnect);
+      socket.on('foo', onFooEvent);
   return (
     <>
       <Router>
@@ -26,8 +50,8 @@ function App() {
               <Route path="Clipr/Messages" element={<Messages/>} />
               <Route path="Clipr/SetReceiver" element={<SetReceiver/>} />
           </Routes>
-      </Router>
-    </>
+      </Router>  
+    </> 
   )
 }
 
