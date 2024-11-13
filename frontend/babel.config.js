4adc1079eed1e3c675dd0666b0d22d46824d21f7
Cesar de Paula <cdepaula942@gmail.com>
2024-11-12 23:18:50 -0800

    Added work-in-progress JEST testing for unit testing

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,5 +2,15 @@ module.exports = {
     presets: [
       '@babel/preset-env',
       ['@babel/preset-react', {runtime: 'automatic'}],
+      {
+
+        targets: {
+
+        node: 'current', 
+
+        },
+
+    }
     ],
+    plugins: [],
   };
