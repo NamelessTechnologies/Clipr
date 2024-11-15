1d8c728bc2a494dd7360f80dad9294ea10a6c7aa
Christopher Lo <christopherlo383@gmail.com>
2024-11-15 00:09:11 -0800

    chore: added test case 21 (get post)

--- CliprBackendTest/PostLiveTest.cs
+++ CliprBackendTest/PostLiveTest.cs
@@ -14,6 +14,9 @@ public class PostLiveTest : IDisposable
 {
     private readonly HttpClient _httpClient = new() { BaseAddress = new Uri("http://localhost:5001") };
 
+
+// ----------------- EXPECTING NOTFOUND -----------------
+
     [Fact]
     public async Task GetPostsOfUser_ReturnsError_WhenUserDoesNotExist()
     {
@@ -24,7 +27,7 @@ public class PostLiveTest : IDisposable
         var content = await response.Content.ReadAsStringAsync();
 
         // Assert
-        Assert.Equal("User not found.", content);
+        Assert.Equal("User not found/No posts.", content);
         this.Dispose();
     }
 
@@ -39,6 +42,31 @@ public class PostLiveTest : IDisposable
 
         // Assert
         Assert.Equal("User not found.", content);
+        this.Dispose();
+    }
+
+// ----------------- EXPECTING OUTPUT -----------------
+
+    [Theory]
+    [InlineData("{\"postID\": 1, \"userID\": 1, \"title\": \"Healthy Living Habits\", \"description\": \"Simple habits that can help you live a healthier life.\", \"datePosted\": \"2023-07-30T00:00:00\", \"mediaType\": \"text\"}")]
+    public async Task GetPost1_ReturnsCorrectJson(string expectedJson)
+    {
+        // Arrange
+        var expectedPosts = JsonSerializer.Deserialize<Post>(expectedJson);
+
+        // Act
+        var response = await _httpClient.GetAsync("/post/1");
+        var content = await response.Content.ReadAsStringAsync();
+        var ActualPosts = JsonSerializer.Deserialize<Post>(content);
+
+        // Assert: Compare the expected and actual Post
+            Assert.Equal(expectedPosts.PostID, ActualPosts.PostID);
+            Assert.Equal(expectedPosts.UserID, ActualPosts.UserID);
+            Assert.Equal(expectedPosts.Title, ActualPosts.Title);
+            Assert.Equal(expectedPosts.Description, ActualPosts.Description);
+            Assert.Equal(expectedPosts.DatePosted, ActualPosts.DatePosted);
+            Assert.Equal(expectedPosts.MediaType, ActualPosts.MediaType);
+
         this.Dispose();
     }
 
