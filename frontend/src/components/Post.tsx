3412f34b9ca942d62dacf82eb85f6957989fe8d2
Justin Nguyen <98506939+chillwafflez@users.noreply.github.com>
2024-12-08 22:30:01 -0800

    Homepage UI refactor (#52)
    * added post description box including character limit and 'show more' feat
    
    * fixed stytling on home page

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -2,17 +2,24 @@ import PostModel from "../types/Post";
 import { LikeIcon } from "./post/LikeIcon";
 import { BookmarkIcon } from "./post/BookmarkIcon";
 import { ShareIcon } from "./post/ShareIcon";
-import lappland from "../assets/lappland.png";
+// import lappland from "../assets/lappland.png";
+import { useState } from "react";
 // import feixiao from "../assets/feixiao_pull.png";
 // import narrow_pic from "../assets/narrow_pic_test.png";
+import father from "../assets/father.jpg"
 
 function Post(props: { postData: PostModel }) {
+
+    const [showMore, setShowMore] = useState(false);
 
     const postUser = props.postData.username;
     const postUserPFP = props.postData.user_pfp;
     const title = props.postData.title;
-    const description = props.postData.content;
+    const description = props.postData.content ?? "";
     const photo_data = props.postData.photo_data;
+
+    const publish_date = props.postData.datePosted?.toLocaleDateString("en-us", { year: 'numeric',month: 'long', day: 'numeric' })
+
     
     const liked = props.postData.liked ?? false;
     const num_likes = props.postData.num_likes ?? 0;
@@ -23,7 +30,7 @@ function Post(props: { postData: PostModel }) {
         <div className="flex-col w-1/2 h-3/5 mr-7 pb-4 rounded-xl">
             {photo_data && (
                 <div className="w-auto bg-neutral-900 rounded-xl">
-                    <img src={lappland}
+                    <img src={father}
                         className="mb-4 h-auto mx-auto rounded-xl"
                         style={{ maxHeight: '70vh'}}>
                     </img>
@@ -40,7 +47,7 @@ function Post(props: { postData: PostModel }) {
                 <div className="flex">
                     <img
                         src={ postUserPFP }
-                        className="w-14 h-14 rounded-full mr-4">
+                        className="w-14 h-14 rounded-full mr-4 shrink-0">
                     </img>
                     <div className="w-full text-white text-2xl mt-3 ">
                         { title }
@@ -55,8 +62,20 @@ function Post(props: { postData: PostModel }) {
                 </div>
             </div>
 
-            <div className="text-amber-500">{ postUser }</div>
-            <div className="text-white text-center">{ description }</div>
+            <div className="text-amber-500">{ postUser }</div>   
+
+            {/* post description */}
+            <div className="flex flex-col text-white mt-1 bg-neutral-900 rounded-xl p-2">
+                <span className="text-white text-sm font-bold">{ publish_date }</span>
+
+                { (showMore == false) ? description.substring(0, 130) : description}
+
+                { description.length > 120 && (
+                    <button onClick={() => setShowMore(!showMore)} className="text-sm text-neutral-300 mt-1 mr-auto">
+                        {showMore ? "Show Less" : "Show More"}
+                    </button>
+                )}
+            </div>
         </div>
     )
 }
