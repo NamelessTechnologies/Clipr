5c809dc7f22a2e08afb32a62902dd12807b0c01b
Daniel Pasion <danielpasion@b01-aruba-authenticated-10-110-202-127.wlan.cpp.edu>
2024-12-09 21:55:32 -0800

    initial message loading"

---
+++ frontend/src/components/messages/IntegratedMessages.tsx
@@ -0,0 +1,190 @@
+import React, { useEffect, useState, useRef } from "react";
+import MessageModel from "../../types/Message";
+import { MessageBox } from "./MessageBox";
+import { socket } from "../../socket";
+import { uri } from "../../App";
+
+function IntegratedMessages(props: {
+  convo_id: string;
+  nickname: string;
+  user_id: string;
+}) {
+  const currentUser = localStorage.getItem("user");
+  const userInfo = currentUser ? JSON.parse(currentUser) : {};
+  const userID = userInfo["user_id"] as number;
+  const userPFP = userInfo["pfp"] as string;
+
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<MessageModel[]>([]);
+
+  const secondUser = props.nickname;
+  const secondUserID = props.user_id;
+  const [secondUserPFP, setSecondUserPFP] = useState("");
+  const convoID = props.convo_id;
+  const [incomingMessage, setIncomingMessage] = useState<MessageModel>();
+
+  const scrollToBottom = useRef<HTMLDivElement | null>(null);
+
+  //Recieving Messages with Sockets
+  useEffect(() => {
+    const handleMessage = (message: MessageModel) => {
+      setIncomingMessage(message);
+    };
+
+    socket.on("recieve-message", handleMessage);
+
+    return () => {
+      socket.off("recieve-message", handleMessage);
+    };
+  }, []);
+
+  // this useEffect contains two functions which fetches the other user's pfp and all messages between the current and other user
+  useEffect(() => {
+    console.log("FETCHING MESSAGES");
+
+    // get and set second user's PFP
+    const recipientPFP = async () => {
+      try {
+        const response = await fetch(`${uri}user/${secondUserID}`);
+        const json = await response.json();
+        setSecondUserPFP(json["pfp"]);
+      } catch (error) {
+        console.error("Error second user's pfp:", error);
+      }
+    };
+    recipientPFP();
+
+    // get and set all messages from convo
+    const fetchMessages = async () => {
+      try {
+        const response = await fetch(
+          `${uri}conversation?User_1=${userID}&User_2=${secondUserID}`
+        );
+        const json = await response.json();
+        const messages: MessageModel[] = json.map((media: MessageModel) => ({
+          id: media.id,
+          convo_id: media.convo_id,
+          content: media.content,
+          datesent: media.datesent,
+          user_id: media.user_id,
+          user_pfp: media.user_pfp,
+        }));
+        setMessages(messages);
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+      }
+    };
+    fetchMessages();
+  }, [secondUserID, userID]);
+
+  useEffect(() => {
+    if (incomingMessage && incomingMessage.convo_id.toString() === convoID) {
+      setMessages((prevMessages) => [...prevMessages, incomingMessage]);
+    }
+  }, [incomingMessage, convoID]);
+
+  // automatically scrolls to bottom of message box to go to most recent message
+  useEffect(() => {
+    scrollToBottom.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  // posts message to backend
+  const postMessage = async () => {
+    const newMessage = {
+      Convo_id: convoID,
+      Content: message,
+      Datesent: new Date(),
+      User_id: userID,
+    };
+
+    try {
+      await fetch(`${uri}conversation/message`, {
+        body: JSON.stringify(newMessage),
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain",
+          "Content-Type": "application/json;charset=UTF-8",
+        },
+      });
+    } catch (error) {
+      console.error("Error posting message:", error);
+    }
+  };
+
+  const sendMessage = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const recentMessage = {
+        convo_id: convoID,
+        content: message,
+        datesent: new Date(),
+        user_id: userID,
+        user_pfp: userPFP,
+      } as unknown as MessageModel;
+      socket.emit("send-message", recentMessage);
+      await postMessage();
+      setMessage("");
+      setMessages((prevMessages) => [...prevMessages, recentMessage]);
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      {/* MESSAGE CONTAINER */}
+      <div className="flex flex-col w-[60vw] h-[85vh] mt-4 justify-center bg-navbar rounded-xl">
+        {/* recipients name on top */}
+        <div className="flex w-full p-4 border-b">
+          <img
+            src={
+              secondUserPFP ||
+              "https://bigskyastrology.com/wp-content/uploads/2013/09/heisenberg-1.jpg"
+            }
+            className="w-11 h-11 rounded-full mr-3"
+          ></img>
+          <h1 className="text-white text-xl my-auto">{secondUser}</h1>
+        </div>
+
+        {/* messages */}
+        <div
+          className="flex flex-col text-white w-full px-5 pb-5 overflow-auto"
+          style={{ height: "72vh", maxHeight: "72vh" }}
+        >
+          {messages.map((msg) => (
+            <MessageBox
+              key={msg.id}
+              username={
+                msg.user_id === userInfo["user_id"]
+                  ? userInfo["username"]
+                  : secondUser
+              }
+              content={msg.content}
+              user_pfp={msg.user_pfp}
+              sender={msg.user_id === userInfo["user_id"] ? true : false}
+            />
+          ))}
+          <div ref={scrollToBottom}></div>
+        </div>
+
+        {/* text box to send your message */}
+        <form className="flex justify-center p-4" onSubmit={sendMessage}>
+          <div className="flex flex-col justify-center w-full mr-3">
+            <input
+              className="p-2 rounded-xl bg-transparent border text-white"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Send Message..."
+            />
+          </div>
+
+          <button className="bg-navbar text-white text-base" type="submit">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default IntegratedMessages;
--- frontend/src/pages/Inbox.tsx
+++ frontend/src/pages/Inbox.tsx
@@ -1,32 +1,86 @@
+import { useState } from "react";
 import ProfilePreview from "../components/messages/ProfilePreview";
+import ShouldBeLoggedIn from "../components/Authenticate";
+import IntegratedMessages from "../components/messages/IntegratedMessages";
 
+interface ProfilePreview {
+  user_id: string;
+  nickname: string;
+  pfp: string;
+  lastMessage: string;
+  convo_id: string;
+}
 function Inbox() {
+  ShouldBeLoggedIn(true);
+  const [selected, setSelected] = useState<string>("1");
+  const [selectedConvoID, setSelectedConvoID] = useState<string>("15");
+  const [selectedNickname, setSelectedNickName] = useState<string>("LebronBon");
+
+  const changeSelected = (
+    user_id: string,
+    nickname: string,
+    convo_id: string
+  ) => {
+    setSelected(user_id);
+    setSelectedNickName(nickname);
+    setSelectedConvoID(convo_id);
+    console.log(selected, selectedConvoID, selectedNickname);
+  };
+
+  const preview: ProfilePreview[] = [
+    {
+      user_id: "1",
+      nickname: "LebronBon",
+      pfp: "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png",
+      lastMessage: "LEBRON JAMES",
+      convo_id: "15",
+    },
+    {
+      user_id: "2",
+      nickname: "Hawk Tuah",
+      pfp: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_9Y6_Qo2SDisnzLkdm3YyVMuoCcwOoIc3VQ&s",
+      lastMessage: "Spit on That Thang!!",
+      convo_id: "21",
+    },
+  ];
+
   return (
     <div className="flex flex-row items-center justify-center">
       {/* Message List */}
-      <div className="flex flex-col w-1/4 mt-4 justify-center bg-navbar rounded-xl">
+      <div className="flex flex-col w-1/4 h-[85vh] mt-4 justify-start bg-navbar rounded-xl overflow-scroll no-scrollbar">
         {/* Header */}
         <div className="flex w-full p-4 border-b">
           <h1 className="text-white text-xl my-auto">Messages</h1>
         </div>
         {/* List Of DMS */}
-        <div className="overflow-scroll">
-          <ProfilePreview
-            user_id="1"
-            nickname="hawkTuah"
-            pfp="https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png"
-            lastMessage="Hey lol"
-          ></ProfilePreview>
-          <ProfilePreview
-            user_id="2"
-            nickname="Spit On That Humengous, Gigantic Thang"
-            pfp="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_9Y6_Qo2SDisnzLkdm3YyVMuoCcwOoIc3VQ&s"
-            lastMessage="Talk Tuah!"
-          ></ProfilePreview>
-        </div>
+        {preview.map((preview) => (
+          <div
+            onClick={() =>
+              changeSelected(
+                preview.user_id,
+                preview.nickname,
+                preview.convo_id
+              )
+            }
+          >
+            <ProfilePreview
+              selected={selected.toString() == preview.user_id}
+              user_id={preview.user_id}
+              nickname={preview.nickname}
+              pfp={preview.pfp}
+              lastMessage={preview.lastMessage}
+            ></ProfilePreview>
+          </div>
+        ))}
       </div>
       {/* TODO Integrate */}
-      <div>Messages Later</div>
+      <div>
+        <IntegratedMessages
+          user_id={selected}
+          nickname={selectedNickname}
+          convo_id={selectedConvoID}
+        ></IntegratedMessages>
+      </div>
     </div>
   );
 }
