5fe1443b2aa229f89e748f3a13e9a7629637966a
Christopher Lo <christopherlo383@gmail.com>
2024-12-09 20:16:19 -0800

    fixed photo upload on editProfile

--- frontend/src/pages/EditProfileForm.tsx
+++ frontend/src/pages/EditProfileForm.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
-import stelle from "../assets/Profile.png";
+import React, { useEffect, useRef, useState } from "react";
 import UserModel from "../types/User";
 import ShouldBeLoggedIn from "../components/Authenticate";
 import { uri } from "../App";
+import ReactS3Client from "react-aws-s3-typescript";
+import { s3Config } from "../components/s3Config";
+import { LuImagePlus } from "react-icons/lu";
+
 
 const EditProfileForm: React.FC = () => {
   ShouldBeLoggedIn(true);
@@ -13,14 +16,36 @@ const EditProfileForm: React.FC = () => {
   const [password, setPassword] = useState(userInfo["password"]);
   const [biography, setBiography] = useState(userInfo["biography"]);
   const [nickname, setNickname] = useState(userInfo["nickname"]);
-  const [pfp, setPfp] = useState(userInfo["pfp"]);
-
+  var pfp = useRef(userInfo["pfp"]);
   const [usernameErrorMsg, setUsernameErrorMsg] = useState("");
   const [emailErrorMsg, setEmailErrorMsg] = useState("");
   const [passwordErrorMsg, setpasswordErrorMsg] = useState("");
 
-  // const navigate = useNavigate();
+  // DATA FOR PFP UPLOAD
+  const [image, setImage] = useState<File | null>(null);
+  const [media_type, setMediaType] = useState("");
+  const [uploadConfirmationMsg,setUploadConfirmationMsg] = useState("");
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files ? e.target.files[0] : null;
+    if(file) {
+      const temp_type = file?.type;
+      const type = temp_type?.split('/')[0];
+      setMediaType(type);
+      setImage(file);
+      console.log("Last uploaded: " + file.name);
+      console.log("media type: " + media_type);
+      setUploadConfirmationMsg("Uploaded " + file.name + "!");
+    }
+  };
 
+  const clickFileInputDIV = (e: React.MouseEvent<HTMLDivElement>) => {
+    const fileInput = document.getElementById('fileInput');
+    if (fileInput) {
+        fileInput.click();
+    }
+  };
+
+  // VALIDATION CHECKS
   const validate = () => {
     let valid = true;
 
@@ -65,12 +90,54 @@ const EditProfileForm: React.FC = () => {
   const createAccount = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     resetErrorMessages();
+    var fileLocation = "";
 
+    // VALIDATE INPUT
     if (!validate()) {
       alert("Error with account details");
       return;
     }
 
+    // ATTEMPT PHOTO UPLOAD
+    if (image) {
+        // VALIDATE FILE
+        if (media_type != "image") {
+            alert("Please upload file of type image!");
+            return;
+        }
+
+        // UPLOAD TO S3
+        const s3 = new ReactS3Client(s3Config);
+        try {
+            console.log("Attempting to upload " + image.name + " of type " + image.type);
+            var fileName = image.name;
+            fileName = fileName.substring(0,fileName.lastIndexOf(".")); // remove the file extension (it will be added by endpoint)
+            const res = await s3.uploadFile(image, fileName);
+            /*
+            * {
+            *   Response: {
+            *     bucket: "bucket-name",
+            *     key: "directory-name/filename-to-be-uploaded",
+            *     location: "https:/your-aws-s3-bucket-url/directory-name/filename-to-be-uploaded"
+            *   }
+            * }
+            */
+            console.log(res);
+            var res_json = JSON.stringify(res);
+            var parsed = JSON.parse(res_json);
+            // console.log("parsed.location: " + parsed.location);
+            fileLocation = parsed.location;
+            pfp.current = fileLocation;
+            alert("Uploaded file to " + fileLocation);
+            // console.log("fileLocation: " + fileLocation);
+        } catch (exception) {
+            console.log(exception);
+        }
+    } 
+
+    console.log("PFP before posting: " + pfp);
+
+    // POST ACCT TO DB
     const newUser = {
       User_id: userInfo["user_id"],
       Username: username,
@@ -78,7 +145,7 @@ const EditProfileForm: React.FC = () => {
       Password: password,
       Biography: biography,
       Nickname: nickname,
-      Pfp: pfp,
+      Pfp: pfp.current,
     };
     try {
       const response = await fetch(uri + "user/", {
@@ -100,6 +167,7 @@ const EditProfileForm: React.FC = () => {
         } catch (error) {
           console.error(error);
         }
+        alert("Success!");
         location.reload();
       } else {
         alert(`${response.status}: ${response.statusText}`);
@@ -110,15 +178,44 @@ const EditProfileForm: React.FC = () => {
     }
   };
 
+
+  if (!userInfo) {
+    return (
+        <div className='text-white'>
+            Loading...
+        </div>
+    );
+}
   return (
     <div className=" bg-navbar flex flex-row justify-center pt-2 ">
       <form
         onSubmit={createAccount}
-        className="bg-navbar rounded px-20 pt-5 pb-5 items-center max-w-md max-h-[88vh] overflow-scroll"
+        className="bg-navbar rounded px-20 pt-5 pb-5 items-center max-w-md max-h-[88vh]"
       >
-        <img src={stelle} className="w-28 h-28 mx-auto"></img>
+        <span className={`block text-white text-xs font-semibold text-center mb-3`}>
+            {uploadConfirmationMsg}
+        </span>
+
+        <div className = 'flex justify-center items-center'>
+            <div className='relative z-0'>
+                <img 
+                    src={pfp.current} 
+                    className="w-28 h-28 mx-auto rounded-full"></img>
+                <input
+                    id='fileInput'
+                    type='file'
+                    accept="image/*"
+                    onChange={handleFileChange}
+                    className='hidden'></input>
+                <div className="absolute inset-0 flex justify-center items-center z-10"
+                    onClick={clickFileInputDIV}>
+                    <LuImagePlus className='w-14 h-14 opacity-75'></LuImagePlus>
+                </div>
+            </div>
+        </div>
+
         <div className="w-full text-amber-500 text-center text-4xl mb-6">
-          Edit {userInfo["username"]}'s Profile
+            Edit {userInfo["username"]}'s Profile
         </div>
 
         <div className="mb-4">
@@ -210,19 +307,7 @@ const EditProfileForm: React.FC = () => {
             placeholder="Nickname"
           />
         </div>
-        <div className="mb-4">
-          <label className="block text-white text-sm font-semibold mb-2">
-            Profile Pic
-          </label>
-          <input
-            type="text"
-            className="border w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline bg-neutral-200"
-            value={pfp}
-            onChange={(e) => setPfp(e.target.value)}
-            required
-            placeholder="Insert Image URL..."
-          />
-        </div>
+        
 
         <div className="flex justify-center mt-5">
           <button
