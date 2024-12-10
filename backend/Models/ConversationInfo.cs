ed7f553f947401ac650094bf4ed389f4c0fc0c9f
Jacob Alonzo <alonzojp@gmail.com>
2024-12-09 22:48:04 -0800

    Implemented conversation list query and route

---
+++ backend/Models/ConversationInfo.cs
@@ -0,0 +1,17 @@
+namespace backend.Models;
+
+public class ConversationInfo
+{
+    public int? Conversation_Id { get; set; }
+
+    public int? Current_User_Id { get; set; }
+
+    public int? Other_User_Id { get; set; }
+
+    public string? Other_User_Nickname { get; set; }
+
+    public string? Latest_Message { get; set; }
+
+    public DateTime? Latest_Message_Date { get; set; }
+
+}
