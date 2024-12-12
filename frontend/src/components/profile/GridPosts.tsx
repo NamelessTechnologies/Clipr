e68d197e6c5ee1c71c6cca68b5fc77a14fa4e09f
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2024-12-11 21:17:06 -0800

    fixed CSS on edit account modal. fixed logic to switch between user's posts and saves on profile page

--- frontend/src/components/profile/GridPosts.tsx
+++ frontend/src/components/profile/GridPosts.tsx
@@ -67,30 +67,26 @@ function GridPosts(props: { profile_id: string }) {
     <div className="flex flex-col w-1/2 mt-10 mx-auto justify-center">
       {/* posts and saves button */}
       <div className="flex justify-center gap-20">
-        <div className="flex items-center">
+        {/* user's posts tab */}
+        <div className="flex items-center cursor-pointer" onClick={() => {if (displayBookmarks) { switchTabs(); } }} >
           {displayBookmarks ? (
             <BsGrid className="w-4 h-4 mr-1 text-white" />
           ) : (
             <BsGridFill className="w-4 h-4 mr-1 text-white" />
           )}
-          <span
-            className={`text-white text-xl cursor-pointer ${!displayBookmarks && "border-b font-bold"}`}
-            onClick={switchTabs}
-          >
+          <span className={`text-white text-xl cursor-pointer ${!displayBookmarks && "border-b font-bold"}`}>
             Posts
           </span>
         </div>
 
-        <div className="flex items-center">
+        {/* user's saves tab */}
+        <div className="flex items-center cursor-pointer" onClick={() => {if (!displayBookmarks) { switchTabs(); } }}>
           {displayBookmarks ? (
             <IoBookmark className="w-4 h-4 mr-1 text-white" />
           ) : (
             <IoBookmarkOutline className="w-4 h-4 mr-1 text-white" />
           )}
-          <span
-            className={`text-white text-xl cursor-pointer ${displayBookmarks && "border-b font-bold"}`}
-            onClick={switchTabs}
-          >
+          <span className={`text-white text-xl cursor-pointer ${displayBookmarks && "border-b font-bold"}`}>
             Saved
           </span>
         </div>
